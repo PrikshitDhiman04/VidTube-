a25479efa6511b4c6ee9a8f1121be1884538b404
Prikshit Dhiman <dhimanprikshit04@gmail.com>
2026-05-23 19:24:06 +0530

    Add Backend Structure Files

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+
+// common middlewares
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(express.static("public"));
+
+
+export { app };
